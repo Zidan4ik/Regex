cd9ca266eebf4cb52bca6c3a25530ecdb02cf598
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-01-21 23:40:06 +0200

    update example about special symbols

--- src/task/example/SpecialSymbosl.java
+++ src/task/example/SpecialSymbosl.java
@@ -3,10 +3,10 @@ package task.example;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SpecialSymbol {
+public class SpecialSymbosl {
     public static void main(String[] args) {
         int year = 2003;
-        String n3 = "[A-z]+\\(+\\d{2}.\\d{2}." + year + "*\\)";
+        String n3 = "[a-zA-z]+\\(+\\d{2}\\.\\d{2}\\.("+year+")+\\)";
         Pattern p3 = Pattern.compile(n3);
         Matcher matcher3 = p3.matcher("День народження друзів:" +
                 "Roma(28.12.2003) Khristina(05.01.2005) Andriy(12.06.2003) Vasya(28.02.2002)" +
